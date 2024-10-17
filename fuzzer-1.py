48e93f8bd1bf8de6f8183f2c32a730c408d0e488
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-10-17 16:50:44 +0530

    Update fuzzer-1.py

--- fuzzer-1.py
+++ fuzzer-1.py
@@ -34,3 +34,43 @@ elif (len(ip_address)>0) and (len(port)>0):
         s.close()
         buffer=buffer+200
         time.sleep(1)
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#!/usr/bin/python3
+
+import sys,socket
+import time 
+
+ip_address=input("Enter the IP address\n")
+port_number=input("Enter the port number\n")
+
+if len(ip_address)==0 or len(port_number)==0:
+    if len(ip_address)>0 and len(port_number)==0:
+        print("*"*50)
+        print("Port number field is empty")
+    elif len(ip_address)==0 and len(port_number)>0:
+        print("*"*50)
+        print("IP address field is empty")                                                                                                                           
+    elif len(ip_address)==0 and len(port_number)==0:                                                                                                                 
+        print("*"*50)                                                                                                                                                
+        print("IP address field is empty")                                                                                                                           
+        print("Port number field is empty")                                                                                                                          
+    else:
+        print("*"*50)
+        print("Issues with the user input")
+elif len(ip_address)>0 and len(port_number)>0:
+    if not port_number.isdigit():
+
+        print("*"*50)
+        print("Port number field must contain only numeric digits")
+    else:
+        buffer_size=100
+        while int(buffer_size)<10000:
+            s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            s.connect((ip_address,int(port_number)))
+            print(f"Sending bufffer size --> {buffer_size}")
+            s.send(b"USER HRISHI" + b"\r\n")
+            s.send(b"PASS " + b"A" * buffer_size + b"\r\n")
+            s.close
+            buffer_size=buffer_size+200
+            time.sleep(1)
